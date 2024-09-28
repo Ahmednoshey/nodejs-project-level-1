9421aae88e09b2c96379e98ad178738e8bfefb17
Ahmed Shoaib <ahmedeldomiaty0@gmail.com>
2024-09-28 15:29:16 +0300

    level1 lesson5

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 const Mydata = require("./models/MydataSchema");
+app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-  res.sendFile("./views/home.html", { root: __dirname });
+  Mydata.find()
+  .then((result) => {res.render("home",{mytitle:"Home Page",arr:result})})
+  .catch((err) => {console.log(err)})
 });
 mongoose
   .connect("mongodb+srv://ahmedeldomiaty0:Bhm4PvQxL0DmLahK@nodejsproject.x786z.mongodb.net/alldata?retryWrites=true&w=majority&appName=nodejsproject")
