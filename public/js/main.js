031620f74ade8eac775740405471732c481f5868
Ahmed Shoaib <ahmedeldomiaty0@gmail.com>
2024-09-30 14:05:35 +0300

    level1 lesson14

---
+++ public/js/main.js
@@ -0,0 +1,43 @@
+if (localStorage.getItem("isSmall") === "yes") {
+  sidebarId.classList.add("small-sidebar");
+} else {
+  sidebarId.classList.remove("small-sidebar");
+}
+
+const toggleSidebar = () => {
+
+  if (localStorage.getItem("isSmall") === "yes") {
+    localStorage.setItem("isSmall", "no");
+    sidebarId.classList.remove("small-sidebar");
+  } else {
+    localStorage.setItem("isSmall", "yes");
+    sidebarId.classList.add("small-sidebar");
+  }
+};
+
+let imgbank = document.getElementById('imgbank');
+let lightmood = document.getElementById('lightmood');
+let darkmood = document.getElementById('darkmood');
+let automood = document.getElementById('automood');
+let myTable = document.getElementById('myTable');
+
+lightmood.onclick = function(){
+  imgbank.src = "../img/1.jpg"
+  myTable.classList.remove("dark");
+}
+
+darkmood.onclick = function(){
+  imgbank.src = "../img/1.png"
+  myTable.classList.add("dark");
+}
+automood.onclick = function(){
+  imgbank.src = "../img/1.png"
+  myTable.classList.add("dark");
+}
+
+
+
+
+function changeimg(element){
+  imgbank.src = "../img/" + element.value + ".png" ;
+}
