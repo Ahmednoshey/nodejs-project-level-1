19ce0d09d06f145de4936a59740d61a7415e5cf1
Ahmed Shoaib <ahmedeldomiaty0@gmail.com>
2024-10-03 10:15:24 +0300

    level1 lesson 18

--- public/js/main.js
+++ public/js/main.js
@@ -47,3 +47,29 @@ function getdates(){
 }
 
 $('#myTable').DataTable();
+
+
+
+
+
+function datevall(dayoffset){
+  let date = new Date();
+let d = date.getDate();
+let m = parseInt(date.getMonth()+1);
+let y = date.getFullYear();
+let ddd = parseInt(date.getDay()+1);
+  if(d < 10){
+    d = "0" + d
+  }
+  if(m < 10){
+    m = "0" + m
+  }
+  if(dayoffset){
+    d = d + +dayoffset;
+  }
+  return y + '-' + m + '-' + d;
+   
+}
+searchbtn.setAttribute("value",datevall());
+
+
