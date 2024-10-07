d01bcc9ecabb55281c86b20bdbc435eeff05d6f5
Ahmed Shoaib <ahmedeldomiaty0@gmail.com>
2024-10-07 13:03:37 +0300

    level1 lesson 18

--- models/SignupSchema.js
+++ models/SignupSchema.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 const authuserSchema = new Schema({
   UserName: String,
   Email: String,
   Password: String,
 },{ timestamps: true });
+authuserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.Password = await bcrypt.hash(this.Password, salt);
+  next();
+ });
 const AuthUser = mongoose.model("User", authuserSchema);
 module.exports = AuthUser;
